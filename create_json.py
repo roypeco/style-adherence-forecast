d232c952a2526df96bd9abf79c7090ec8a1385fe
roypeco <roy.peco@gmail.com>
2024-06-09 21:50:00 +0900

    SES2024最新版

--- create_json.py
+++ create_json.py
@@ -2,6 +2,16 @@ import pandas as pd
 import json
 
 result_dict = {}
+# all_id = {}
+black_list = [
+  'C0209',
+  'W1514',
+  'E0401',
+  'W0611',
+  'R1705',
+  'R1732',
+  'W0707',
+]
 
 with open("dataset/white_list.txt") as f:
   project_list = f.read().splitlines()
@@ -11,34 +21,47 @@ for file_name in project_list[0:61]:
               "c":[0, 0],
               "e":[0, 0],
               "r":[0, 0],
-              "i":[0, 0],
-              "f":[0, 0]}
+              }
   
   df_label = pd.read_csv(f'./dataset/outputs/{file_name}_label.csv', header=None)
   df_value = pd.read_csv(f'./dataset/outputs/{file_name}_value.csv')
   df_wid = df_value["Warning ID"]
   for wid, label in zip(df_wid.to_list(), df_label[0]):
-    if "W" in wid:
+    if "W" in wid and not wid in black_list:
       fix_dict["w"][0] += label
       fix_dict["w"][1] += 1
-    elif "C" in wid:
+    elif "C" in wid and not wid in black_list:
       fix_dict["c"][0] += label
       fix_dict["c"][1] += 1
-    elif "E" in wid:
+    elif "E" in wid and not wid in black_list:
       fix_dict["e"][0] += label
       fix_dict["e"][1] += 1
-    elif "R" in wid:
+    elif "R" in wid and not wid in black_list:
       fix_dict["r"][0] += label
       fix_dict["r"][1] += 1
-    elif "I" in wid:
-      fix_dict["i"][0] += label
-      fix_dict["i"][1] += 1
-    elif "F" in wid:
-      fix_dict["f"][0] += label
-      fix_dict["f"][1] += 1
+    # 類似度の差分を出すために消す
+    # elif "I" in wid:
+    #   fix_dict["i"][0] += label
+    #   fix_dict["i"][1] += 1
+    # elif "F" in wid:
+    #   fix_dict["f"][0] += label
+    #   fix_dict["f"][1] += 1
+    
+    # if wid in all_id:
+    #   if all_id[wid][2]:
+    #     all_id[wid][0] += 1
+    #     all_id[wid][2] = False
+    # else:
+    #   all_id[wid] = [1, 62, False]
     
   result_dict[f"{file_name}"] = fix_dict
+  
+  # for flg in all_id:
+  #   all_id[flg][2] = True
 
 # print(json.dumps(result_dict, indent=4))
-with open('dataset/test_output.json', 'w') as f:
-    json.dump(result_dict, f, indent=2)
+# with open('dataset/test_output.json', 'w') as f:
+#     json.dump(result_dict, f, indent=2)
+
+# sorted_dict = sorted(all_id.items(), key=lambda x:x[1][0], reverse=True)
+# print(sorted_dict)
---
+++ show_importance.py
@@ -0,0 +1,110 @@
+from src.modules import model_loader
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import confusion_matrix
+import matplotlib.pyplot as plt
+import pandas as pd
+import collections
+import csv
+
+model_name = "Logistic"  # Logistic, RandomForest, SVM の３種類から選ぶ
+dummys = model_loader.get_dummy()
+project_name = "schema_salad"
+id_dict = {}
+list1 = []
+list2 = []
+list3 = []
+list4 = []
+model_all = model_loader.load_model("merge", model_name)
+model_one = model_loader.load_model("conventional", model_name, project_name=project_name)
+# model_dict = model_loader.load_model("cross", model_name)
+
+df_value = pd.read_csv(f"./dataset/outputs/{project_name}_value.csv")
+df_label = pd.read_csv(f"./dataset/outputs/{project_name}_label.csv", header=None)
+df_one = pd.concat(
+        [
+            pd.get_dummies(df_value["Warning ID"]),
+            df_value.drop(columns="Warning ID"),
+        ],
+        axis=1,
+    )
+not_dummy_t, not_dummy = train_test_split(df_value, test_size=0.2, shuffle=False)
+_, test_value = train_test_split(df_one, test_size=0.2, shuffle=False)
+_, test_label = train_test_split(df_label, test_size=0.2, shuffle=False)
+for i in list(dummys):
+    id_dict[i] = []
+for wid in not_dummy["Warning ID"]:
+    if wid in id_dict:
+        id_dict[wid].append(1)
+        for i in id_dict:
+            id_dict[i].append(0)
+        id_dict[wid].pop(-1)
+    else:
+        for i in id_dict:
+            id_dict[i].append(0)
+id_df = pd.DataFrame(id_dict)
+not_dummy = not_dummy.reset_index(drop=True)
+test_df = pd.concat([id_df, not_dummy], axis=1)
+metric_list = list(not_dummy.columns)
+metric_one_list = list(df_one.columns)
+label = dummys + metric_list
+pre_list = model_one.predict(test_value.drop(["Project_name"], axis=1))
+pre_list2 = model_all.predict(
+        test_df.drop(["Warning ID", "Project_name"], axis=1)
+    )
+label.remove("Warning ID")
+label.remove("Project_name")
+metric_one_list.remove("Project_name")
+
+if model_name == "Logistic":
+    importances_all = model_all.coef_.squeeze().tolist()
+else:
+    importances_all = model_all.feature_importances_
+dic = dict(zip(label, importances_all))
+
+if model_name == "Logistic":
+    importances = model_one.coef_.squeeze().tolist()
+else:
+    importances = model_one.feature_importances_
+
+dic_all = dict(zip(label, importances_all))
+dic = dict(zip(metric_one_list, importances))
+
+sorted_dict_all = dict(sorted(dic_all.items(), key=lambda item: abs(item[1])))
+sorted_dict = dict(sorted(dic.items(), key=lambda item: abs(item[1])))
+length = 10
+keys_all = list(sorted_dict_all.keys())
+r_keys_all = list(reversed(keys_all))
+values_all = list(sorted_dict_all.values())
+keys = list(sorted_dict.keys())[-length:]
+values = list(sorted_dict.values())[-length:]
+
+# for i in reversed(keys):
+#     print(f"従来:{i} -> {r_keys_all.index(i)+1}")
+
+for i, j, k, l in zip(not_dummy["Warning ID"].to_list(), test_label[0].to_list(), pre_list, pre_list2):
+    if j == k:
+       list1.append(i)
+    if j == l:
+        list2.append(i)
+    list3.append(i)
+
+for i in not_dummy_t["Warning ID"].to_list():
+    list4.append(i)
+
+print("従来")
+print(collections.Counter(list1))
+print("提案")
+print(collections.Counter(list2))
+print("テストデータ")
+print(collections.Counter(list3))
+print("学習データ")
+print(collections.Counter(list4))
+# print(confusion_matrix(test_label[0].to_list(), pre_list))
+# print(confusion_matrix(test_label[0].to_list(), pre_list2))
+
+# 図示
+# plt.figure(figsize = (10,6))
+# plt.barh(y = range(len(values)), width = values)
+# plt.yticks(ticks = range(len(keys)), labels = keys)
+# # plt.savefig(f'merge_{model_name}.png')
+# plt.show()
